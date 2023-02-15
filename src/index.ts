88e882004d6cd5cab84352267df528f420497d50
uu-z <ncysatnaf@gmail.com>
2023-02-16 00:51:33 +0800

    bugfix: remove dotenv & bump version to 0.1.12

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
 import BigNumber from 'bignumber.js';
 import dayjs from 'dayjs';
 import { Signer } from 'ethers';
